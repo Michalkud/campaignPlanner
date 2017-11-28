3aded316e1f091567b75bdc86737805361f540ef
Petr <petrstojanov@gmail.com>
2017-11-28 22:42:24 +0100

    Collabsible menu, header styling

--- src/components/UserPanel/UserPanel.js
+++ src/components/UserPanel/UserPanel.js
@@ -12,14 +12,18 @@ class UserPanel extends Component {
     location.reload();
   }
 
-  content = () => 
-  (<Button onClick={this._logout}>Odhlásit se</Button>)
-  
+  content = () =>
+  (<div>
+    <Button onClick={this._logout}>Odhlásit se</Button>
+   </div>)
+
   render() {
     return (
       <div style={{ marginTop: '10px' }}>
-        <Popover placement="bottom" title={'User panel'} content={this.content()} trigger="click">
-          <Avatar size="large" src={this.props && this.props.user && this.props.user.picture} />
+        <Popover placement="bottom" title={this.props.user.name}
+            content={this.content()} trigger="click">
+          <Avatar size="large"
+            src={this.props && this.props.user && this.props.user.picture} />
         </Popover>
       </div>
     );
