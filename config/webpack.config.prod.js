595246491dfb660e14b57f226dd0fc7d4f667ed3
Petr Stojanov <petrstojanov@gmail.com>
2018-03-14 21:47:40 +0100

    Fix in webpack.config.prod.js

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -90,7 +90,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      config: path.join(__dirname, '../src/config.prod.js')
+      config: path.join(__dirname, '../src/config.prod.js'),
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
